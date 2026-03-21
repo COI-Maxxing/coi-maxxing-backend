b480b2e8481569cf3ecdb902f050f2364c55513b
seyann16 <ianjames.calumpang@gmail.com>
2026-03-21 16:36:03 +0800

    feat(api): new api endpoints for subcontractors and documents

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\SubcontractorController;
 
+// auth endpoints
 Route::prefix('auth')->group(function () {
     // login route
     Route::post('/login', [AuthController::class, 'login']);
@@ -14,5 +17,24 @@ Route::prefix('auth')->group(function () {
 
         // authenticated user route
         Route::get('/me', [AuthController::class, 'me']);
+    });
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    // subcontractor endpoints
+    Route::prefix('subcontractors')->group(function () {
+        Route::get('/', [SubcontractorController::class, 'index']);
+        Route::post('/', [SubcontractorController::class, 'store']);
+        Route::get('/{id}', [SubcontractorController::class, 'show']);
+        Route::delete('/{id}', [SubcontractorController::class, 'destroy']);
+        Route::get('/{subcontractorId}/documents', [DocumentController::class, 'index']);
+    });
+
+    // document endpoints
+    Route::prefix('documents')->group(function () {
+        Route::post('/', [DocumentController::class, 'store']);
+        Route::get('/{id}', [DocumentController::class, 'show']);
+        Route::patch('/{id}/validate', [DocumentController::class, 'confirm']);
+        Route::patch('/{id}/reject', [DocumentController::class, 'reject']);
     });
 });
